54f4319a5dd6fa91d088874fa60d9fe1a1f2870f
Mandalore1077 <jihall77@outlook.com>
2023-05-24 16:03:51 -0400

    add classes

---
+++ WitchKnightClass/Archtypes/WitchKing.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WitchKnightClass.Archtypes
+{
+    internal class WitchKing
+    {
+        /*
+         *  class feature outline:
+         *      Ring of Power
+         *      *   Item starts equiped, cant be removed
+         *      *   aura
+         *          *   Xadd char modifier to all alied attack and fortitude save rollsX
+         *          *   add char modifier to alied undead/fiend attack and fortitude save rolls
+         *          *   undead also gain bonus to AC and damage
+         *          *   minus char modifier to will saves of living non-you creatures within aura including allies
+         *      *   add 1/2 ranks to intimidation
+         *      *   add 3/4 ranks in class to charisma
+         *      *   minus ranks to diplomacy
+         *      *   effects add slowly over progression, progression also increase aura radius
+         *      Morgul Blade
+         *      *   special attack [function] per day uses
+         *      *   standard damage roll plus effects
+         *          *   fail save causes high instant damage and DoT
+         *          *   pass save only DoT
+         *          *   target dies while under morgul poison automatically resurected as allied zombie
+         *      *   starts as full round action upgrades to attack action Xthen swift actionX
+         *      Fell Beast/Bane Weapon
+         *      Deity -- restricted
+         *      XDomain selection x2X
+         *      Domain Selection -- restricted
+         *      charisma arcane caster
+         *      *   Xpaladin spell levelsX
+         *      *   alchemist spell levels
+         *      *   wizard/necromancer spell list
+         *      alignment restriction
+         *      Command undead
+         *      *   undead in aura have disadvantage
+         *      *   start as single target upgrade to AoE
+         *      *   Xfull round actionX
+         *      *   move action
+         *      Bonus combat feats X4,8,12,X16
+         *      Free feats without reqs
+         *      *   two weapon fighting at lvl 4
+         *      *   improved two weapon fightin at lvl 8
+         *      *   greater two weapon fighting at lvl 12
+         *      Oracle curse wasting
+         *      Additional capstone option - become wraith
+         *
+         */
+        public static void AddWitchKing()
+        {
+            
+        }
+    }
+}
